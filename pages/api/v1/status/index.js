852956cb03e0513a0918a25dd603ea3bd41e7e21
Áller Augusto de Souza <allerasouza@gmail.com>
2024-10-23 22:03:30 -0300

    feat: finishi `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,36 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const databaseVersion = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+  const databaseMaxConnections = parseInt(
+    (await database.query("SHOW max_connections;")).rows[0].max_connections
+  );
+
+  // SQL INJECTION Example
+  // const databaseOpenedConnections = (
+  //   await database.query(
+  //     `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname= '${request.query.databaseName}';`
+  //   )
+  // ).rows[0].count;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = (
+    await database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname= $1;",
+      values: [databaseName],
+    })
+  ).rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: databaseMaxConnections,
+        opened_connections: databaseOpenedConnections,
+      },
+    },
   });
 }
 
